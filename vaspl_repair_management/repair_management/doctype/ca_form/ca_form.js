9940a744ddd5939020be8e0df8240284d1ba49da
devops_vijatshi <devops@vijatshisoftware.com>
2023-03-21 13:01:01 +0000

    safety doc type moved to repair management

---
+++ vaspl_repair_management/repair_management/doctype/ca_form/ca_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Vijatshi Software and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('CA Form', {
+	// refresh: function(frm) {
+
+	// }
+});
